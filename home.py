da9f3d3785d9bf1d26ff98706d612bdee4b398cb
simonebianco <82176085+simonebianco@users.noreply.github.com>
2023-11-16 12:00:22 +0100

    Add files via upload

--- home.py
+++ home.py
@@ -1,15 +1,10 @@
-import os
 import pandas as pd
 import streamlit as st
-import time
 from PIL import Image
 
-# generate random integer values
-from random import seed
-from random import randint
 
 # data frame
-def create_df():
+def create_df_3notes():
     note = {'Note': ['DO', 'REb', 'RE', 'MIb', 'MI', 'FA', 'FA#', 'SOL', 'LAb', 'LA', 'SIb', 'SI']}
     posizione = {'Posizione': ['Fondamentale', 'Primo Rivolto', 'Secondo Rivolto']}
     stato = {'Stato': ['Maggiore', 'Minore', 'Aumentato', 'Diminuito']}
@@ -25,45 +20,47 @@ def create_df():
     df['Accordo'] = df['Note'] + ' ' + df['Stato'] + ' ' + df['Posizione']
     return df
 
-df = create_df()
+# data frame
+def create_df_4notes():
+    note = {'Note': ['DO', 'REb', 'RE', 'MIb', 'MI', 'FA', 'SOLb', 'SOL', 'LAb', 'LA', 'SIb', 'SI']}
+    posizione = {'Posizione': ['Fondamentale', 'Primo Rivolto', 'Secondo Rivolto', 'Terzo Rivolto']}
+    stato = {'Stato': ['Settima Maggiore']}
+    note_df = pd.DataFrame(note)
+    posizione_df = pd.DataFrame(posizione)
+    stato_df = pd.DataFrame(stato)
+    note_df['key'] = 1
+    posizione_df['key'] = 1
+    stato_df['key'] = 1
+    df = pd.merge(note_df, posizione_df, on ='key')
+    df = pd.merge(df, stato_df, on ='key')
+    df.drop('key', axis=1, inplace=True)
+    df['Accordo'] = df['Note'] + ' ' + df['Stato'] + ' ' + df['Posizione']
+    return df
+
+df_3 = create_df_3notes()
+df_4 = create_df_4notes()
 
 # pagina
 st.title('Chords Generator App')
 st.markdown("____")
-text = 'This app allows you to do exercises on the position and composition of triad chords on the piano, including all the turns. Selecting the checkbox automatically generates a chord, the solution of which can be seen later with the specific button. In the perimeter of random chords there are 144 possibilities, representing the 12 notes, in 3 turns for 4 states (Major, Minor, Augmented, Diminished).'
+text = 'This app allows you to do exercises on the position and composition of chords on the piano, including all the turns. Using the selection boxes below you can see all the chords contained in the application, with the corresponding picture of the location. On the pages to the right, however, you can do exercises by selecting a chord randomly'
 st.write(text)
 st.markdown("____")
-st.write('Select the checkbox for generate a random chord')
+
+# boxes
+option_3 = st.selectbox('3 notes chords list:',(df_3['Accordo'].unique()), placeholder='Select one chord to visualize the position',index=None)
+if option_3 != None:
+    image_url = "{}\{}.jpg".format(option_3.split(' ')[1], option_3)
+    image = Image.open(image_url)
+    st.image(image, width=700) 
+
+st.markdown(" ")
+st.markdown(" ")
 st.markdown(" ")
 
-# initializing with a random number
-if "rn" not in st.session_state:
-    st.session_state["rn"] = randint(0, len(df['Accordo']))
-
-# callback function to change the random number stored in state
-def change_number():
-    st.session_state["rn"] = randint(0, len(df['Accordo']))
-    return
-index =  st.session_state.rn
-Accordo_random = df['Accordo'].iloc[index]
-Stato_Accordo_random = df['Stato'].iloc[index]
-
-# process
-if st.checkbox("Generate", on_change=change_number):
-    progress = st.progress(0)
-    for i in range(100):
-        time.sleep(0.01)
-        progress.progress(i+1)
-    st.write(Accordo_random)
-    st.markdown(" ")
-
-    if st.button("Solution"):
-        image_url = "{}/{}.jpg".format(Stato_Accordo_random, Accordo_random)
-        image = Image.open(image_url)
-        st.image(image, width=700) 
-
-
-
-
-
+option_4 = st.selectbox('4 notes chords list:',(df_4['Accordo'].unique()), placeholder='Select one chord to visualize the position',index=None)
+if option_4 != None:
+    image_url = "{}\{}.jpg".format(option_4.split(' ')[1] + ' ' + option_4.split(' ')[2], option_4)
+    image = Image.open(image_url)
+    st.image(image, width=700) 
 
