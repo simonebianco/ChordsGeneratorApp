25116271a27e19020cfe0c8664138b5e8e545e4d
simonebianco <82176085+simonebianco@users.noreply.github.com>
2024-01-04 14:10:44 +0100

    Update home.py

--- home.py
+++ home.py
@@ -37,8 +37,22 @@ def create_df_4notes():
     df['Accordo'] = df['Note'] + ' ' + df['Stato'] + ' ' + df['Posizione']
     return df
 
+# data frame
+def create_df_scales():
+    note = {'Note': ['DO', 'REb', 'RE', 'MIb', 'MI', 'FA', 'FA#', 'SOL', 'LAb', 'LA', 'SIb', 'SI']}
+    stato = {'Stato': ['Maggiore']}
+    note_df = pd.DataFrame(note)
+    stato_df = pd.DataFrame(stato)
+    note_df['key'] = 1
+    stato_df['key'] = 1
+    df = pd.merge(note_df, stato_df, on ='key')
+    df.drop('key', axis=1, inplace=True)
+    df['Scala'] = df['Note'] + ' ' + df['Stato']
+    return df
+
 df_3 = create_df_3notes()
 df_4 = create_df_4notes()
+df_sc = create_df_scales()
 
 # pagina
 st.title('Chords Generator App')
@@ -50,9 +64,9 @@ st.markdown("____")
 # boxes
 option_3 = st.selectbox('3 notes chords list:',(df_3['Accordo'].unique()), placeholder='Select one chord to visualize the position',index=None)
 if option_3 != None:
-    image_url = "{}/{}.jpg".format(option_3.split(' ')[1], option_3)
-    image = Image.open(image_url)
-    st.image(image, width=700) 
+    image_url_3 = "{}/{}.jpg".format(option_3.split(' ')[1], option_3)
+    image_3 = Image.open(image_url_3)
+    st.image(image_3, width=700) 
 
 st.markdown(" ")
 st.markdown(" ")
@@ -60,7 +74,18 @@ st.markdown(" ")
 
 option_4 = st.selectbox('4 notes chords list:',(df_4['Accordo'].unique()), placeholder='Select one chord to visualize the position',index=None)
 if option_4 != None:
-    image_url = "{}/{}.jpg".format(option_4.split(' ')[1] + ' ' + option_4.split(' ')[2], option_4)
-    image = Image.open(image_url)
-    st.image(image, width=700) 
+    image_url_4 = "{}/{}.jpg".format(option_4.split(' ')[1] + ' ' + option_4.split(' ')[2], option_4)
+    image_4 = Image.open(image_url_4)
+    st.image(image_4, width=700) 
+
+st.markdown(" ")
+st.markdown(" ")
+st.markdown(" ")   
+
+option_sc = st.selectbox('Scales list:',(df_sc['Scala'].unique()), placeholder='Select one scale to visualize the position',index=None)
+if option_sc != None:
+    image_url_sc = "{}/{}.jpg".format('Scale', option_sc)
+    image_sc = Image.open(image_url_sc)
+    st.image(image_sc, width=700) 
+
 
